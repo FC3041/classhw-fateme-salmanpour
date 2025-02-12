1305128c3b16eab8e522470fab2c2dc8b0245e0c
najme <najmesalmanpour@gmail.com>
2025-02-12 16:54:44 +0330

    vance part1

---
+++ S01/HW/L2.2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace L2._2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        head(args);
+        line(args);
+        body(args);
+        line(args);
+        body(args);
+        line(args);
+        head(args);
+    }
+
+    static void head(string[] args)
+    {
+        Console.WriteLine("     ^      ");
+        Console.WriteLine("    /|\\     ");
+        Console.WriteLine("   //|\\\\   ");
+        Console.WriteLine("  ///|\\\\\\   ");
+    }
+
+    static void body(string[] args)
+    {
+        Console.WriteLine(" +*******+  ");
+        Console.WriteLine(" +*******+  ");
+        Console.WriteLine(" +*******+  ");
+        Console.WriteLine(" +*******+  ");
+    }    
+
+    static void line(string[] args)
+    {
+         Console.WriteLine(" +-------+  ");
+    }
+}
+
+
+
